b3f6d4ce9a489e56555d26a11f49645a444c3196
Tavee Khunbida <taveek@gmail.com>
2016-04-30 22:14:50 +0700

    initial commit

---
+++ src/main/java/com/taveek/admin/SpringBootAdminApplication.java
@@ -0,0 +1,12 @@
+package com.taveek.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAdminApplication.class, args);
+	}
+}
